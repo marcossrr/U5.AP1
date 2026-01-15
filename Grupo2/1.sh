37f7b820ac3d8e355c7b5104cb836d7fd86e5a39
Marcos <mariro@alumnatflorida.es>
2026-01-15 09:11:33 +0100

    Ejercicio 1 g2

---
+++ Grupo2/1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Introduce 2 cadenas de caracteres"
+read var1 var2
+
+if($var1 = $var2) {
+    echo "Tus cadenas son iguales";
+}else{
+    echo "Tus cadenas son diferentes";
+}
